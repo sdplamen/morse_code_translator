3144582a9b19822cbca7d060692872b87faa36ae
Plamen Stoilkov <sdplamen@gmail.com>
2024-07-13 19:18:18 +0300

    Add code.

---
+++ morse_code.py
@@ -0,0 +1,17 @@
+morse_dict = {
+        '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F',
+        '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L',
+        '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R',
+        '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X',
+        '-.--': 'Y', '--..': 'Z', '.----': '1', '..---': '2', '...--': '3',
+        '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8',
+        '----.': '9', '-----': '0'
+    }
+
+code = input().split(' | ') # if words separated by pipe
+message = []
+for word in code:
+    letters = word.split() # if lettres separated by space
+    norm_word = ''.join([morse_dict[letter] for letter in letters])
+    message.append(norm_word)
+print(' '.join(message))
