8b4caf71852ab86641442ff4b46e439f398fe3b7
Plamen Stoilkov <sdplamen@gmail.com>
2025-10-14 18:04:07 +0300

    Prepare for deploy

--- morse_code/settings.py
+++ morse_code/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-g(0z_vw%dotk1mg9%6&2+s&%#uyl3!9svfhf*ao+myyvo-9dy#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['127.0.0.1','localhost','morse-code-translator.onrender.com']
 
 
 # Application definition
